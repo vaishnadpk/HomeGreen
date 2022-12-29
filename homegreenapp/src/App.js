3a6f50b33ec9f658d1625143c25ffc6714f8d0d0
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-29 13:48:29 +0100

    Removed unwanted files and added components

---
+++ homegreenapp/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import TopHeader from "./components/topHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TopHeader />
+      </header>
+    </div>
+  );
+}
+
+export default App;
