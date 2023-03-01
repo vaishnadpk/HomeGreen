150f00bad5c88b72bb0d311074ad849877b3fe83
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2023-03-01 20:18:35 +0100

    updated

---
+++ build/precache-manifest.d8553acc6bb0bf65675b65e138d77dfc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff1d731e4a95fff32cdf",
+    "url": "/vaishnadpk/homegreen/static/css/main.a270a173.chunk.css"
+  },
+  {
+    "revision": "ff1d731e4a95fff32cdf",
+    "url": "/vaishnadpk/homegreen/static/js/main.afd74b6a.chunk.js"
+  },
+  {
+    "revision": "cbfe25b729aefca90edf",
+    "url": "/vaishnadpk/homegreen/static/js/runtime~main.32ac6951.js"
+  },
+  {
+    "revision": "14ce920b78068097df0a",
+    "url": "/vaishnadpk/homegreen/static/js/2.59b8de67.chunk.js"
+  },
+  {
+    "revision": "c5bc574bdd0b53bea9f24d680950bd71",
+    "url": "/vaishnadpk/homegreen/static/media/logo.c5bc574b.png"
+  },
+  {
+    "revision": "9a9c67c87ea95a205c82dc5899f78cf6",
+    "url": "/vaishnadpk/homegreen/static/media/Fiscuselastica.9a9c67c8.jpeg"
+  },
+  {
+    "revision": "aeff697920483f04d1da2a05e94b1001",
+    "url": "/vaishnadpk/homegreen/static/media/Goldenpalm.aeff6979.jpeg"
+  },
+  {
+    "revision": "641768d243772ea5cd5bebf6a1ab6f51",
+    "url": "/vaishnadpk/homegreen/static/media/PhilodendronRojoCongo.641768d2.jpeg"
+  },
+  {
+    "revision": "fcc0dc267d465411a83cce7840894933",
+    "url": "/vaishnadpk/homegreen/static/media/Tulips 10 pack.fcc0dc26.jpeg"
+  },
+  {
+    "revision": "9b0ecc87cff38f955d72ff4ce833c20f",
+    "url": "/vaishnadpk/homegreen/static/media/topimage 1.9b0ecc87.jpeg"
+  },
+  {
+    "revision": "10a2ae1628de24f9cff74f02af29c3a3",
+    "url": "/vaishnadpk/homegreen/static/media/topimage2.10a2ae16.jpeg"
+  },
+  {
+    "revision": "30fb838054f70c3af9e4240adb64ae45",
+    "url": "/vaishnadpk/homegreen/static/media/topimage3.30fb8380.jpeg"
+  },
+  {
+    "revision": "cc7386a9d2b29355a9aac47bf8964385",
+    "url": "/vaishnadpk/homegreen/static/media/h-min.cc7386a9.jpeg"
+  },
+  {
+    "revision": "53667adebe7ae71f258761e91cb3bac8",
+    "url": "/vaishnadpk/homegreen/index.html"
+  }
+];
