3b3ae31e0179417c13522219550b020c0c121889
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2023-03-01 21:31:06 +0100

    updated

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vaishnadpk/homegreen/precache-manifest.4006b3e6a63ad6ecf3b6ad57666fbbd5.js"
+  "./precache-manifest.7856b01a01eacda09499eaf0a3018f42.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vaishnadpk/homegreen/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
