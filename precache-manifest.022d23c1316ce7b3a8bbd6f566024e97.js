82d389738484a38389035f8cdf9f101904b8f586
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2023-03-01 20:48:00 +0100

    Updates

---
+++ precache-manifest.022d23c1316ce7b3a8bbd6f566024e97.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bfdee76c3db88ea4a05",
+    "url": "./static/css/main.a7753e3e.chunk.css"
+  },
+  {
+    "revision": "6bfdee76c3db88ea4a05",
+    "url": "./static/js/main.6bfdee76.chunk.js"
+  },
+  {
+    "revision": "98dc7e8fa4cd697a6177",
+    "url": "./static/js/1.98dc7e8f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c5bc574bdd0b53bea9f24d680950bd71",
+    "url": "./static/media/logo.c5bc574b.png"
+  },
+  {
+    "revision": "9a9c67c87ea95a205c82dc5899f78cf6",
+    "url": "./static/media/Fiscuselastica.9a9c67c8.jpeg"
+  },
+  {
+    "revision": "aeff697920483f04d1da2a05e94b1001",
+    "url": "./static/media/Goldenpalm.aeff6979.jpeg"
+  },
+  {
+    "revision": "641768d243772ea5cd5bebf6a1ab6f51",
+    "url": "./static/media/PhilodendronRojoCongo.641768d2.jpeg"
+  },
+  {
+    "revision": "fcc0dc267d465411a83cce7840894933",
+    "url": "./static/media/Tulips 10 pack.fcc0dc26.jpeg"
+  },
+  {
+    "revision": "9b0ecc87cff38f955d72ff4ce833c20f",
+    "url": "./static/media/topimage 1.9b0ecc87.jpeg"
+  },
+  {
+    "revision": "10a2ae1628de24f9cff74f02af29c3a3",
+    "url": "./static/media/topimage2.10a2ae16.jpeg"
+  },
+  {
+    "revision": "30fb838054f70c3af9e4240adb64ae45",
+    "url": "./static/media/topimage3.30fb8380.jpeg"
+  },
+  {
+    "revision": "cc7386a9d2b29355a9aac47bf8964385",
+    "url": "./static/media/h-min.cc7386a9.jpeg"
+  },
+  {
+    "revision": "f400469cb01ed13f1eddd7aebd6f6efc",
+    "url": "./index.html"
+  }
+];
